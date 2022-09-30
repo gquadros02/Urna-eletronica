fb15bb6e630f8acadbfb6af312a5259c6f370df5
unknown <gleidsonquadros@live.com>
2022-09-29 21:46:36 -0300

    Add urna

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+   {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+        {
+            numero: '17888',
+            nome:"Arancio Gomes",
+            partido: "PSL",
+            fotos:[
+                {url:'17888.jpg', legenda: 'Vereador'}
+            ]
+        },
+        {
+            numero: '12512',
+            nome:"Bernardo Guedes",
+            partido: "PDTL",
+            fotos:[
+                {url:'12512.jpg', legenda: 'Vereador'} 
+            ]
+        },
+    ]
+   },
+   {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [
+            {
+            numero: '22',
+            nome:"Pedro Augusto",
+            partido: "PL",
+            vice: 'Totonho Pimentel',
+            fotos:[
+                {url:'22.jpg', legenda: 'Prefeito'},
+                {url:'22_2.jpg', legenda: 'Vice-Prefeito', small: true} 
+             ]
+             },
+            {
+            numero: '40',
+            nome:"Ricardo Paf Pax",
+            partido: "PSB",
+            vice: 'Marco Antonio Lacerda',
+            fotos:[
+                {url:'40.jpg', legenda: 'Prefeito'}, 
+                {url:'40_2.jpg', legenda: 'Vice-Prefeito', small: true} 
+                ]
+            },
+         ]
+    }
+];
